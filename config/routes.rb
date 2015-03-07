ccf525386a9aaa30301df8f6bab85515dcd4a128
Иван Бишевац <ivan.bisevac@gmail.com>
2015-03-07 14:50:55 +0100

    o-nama, adds correct link to page and populates page with initial data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'o_nama', to: 'static_pages#o_nama'
   get 'kontakt', to: 'static_pages#kontakt'
   post 'send_kontakt_email', to: 'static_pages#send_kontakt_email'
   root to: 'static_pages#naslovna'
